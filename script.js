a3a63c1936df5790b54882221217db1e3f57e96a
achirawitthamnao-collab <achirawit.thamnao@e-tech.ac.th>
2025-12-27 23:16:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("registerForm").addEventListener("submit", function(e) {
-    const email = document.getElementById("email").value;
-
-    if (email === "admin@gmail.com") {
-        e.preventDefault();
-        window.location.href = "admin.html";
-    }
-});
